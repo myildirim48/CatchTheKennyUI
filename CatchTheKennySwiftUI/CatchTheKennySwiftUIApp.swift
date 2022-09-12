cacfd1dbd6276fc6005651bcf869661aa8a6ba42
Muhammed YILDIRIM <muhammedyildirim48@gmail.com>
2022-09-13 00:08:41 +0300

    Initial Commit

---
+++ CatchTheKennySwiftUI/CatchTheKennySwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchTheKennySwiftUIApp.swift
+//  CatchTheKennySwiftUI
+//
+//  Created by YILDIRIM on 13.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct CatchTheKennySwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
